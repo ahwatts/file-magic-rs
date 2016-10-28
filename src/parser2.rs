43eff5ec8fd6d41c036d09c6a38a711575ef0ed0
Andrew Watts <ahwatts@gmail.com>
2016-10-27 23:22:50 -0400

    Working with combine.

--- src/parser2.rs
+++ src/parser2.rs
@@ -1,15 +1,33 @@
 use combine::*;
 use combine::char::*;
-use entry::MagicEntry;
+use entry::*;
 use std::io::{BufRead, BufReader, Read};
+use std::str::FromStr;
 
 pub fn parse<R: Read>(input: &mut R) -> Result<Vec<MagicEntry>, String> {
-    let entries = Vec::new();
+    let mut entries = Vec::new();
     let buf_input = BufReader::new(input);
 
     for (line_num_minus_one, line_rslt) in buf_input.lines().enumerate() {
-        let _line_num = line_num_minus_one + 1;
-        let _line = try!(line_rslt.map_err(|e| format!("I/O Error: {}", e)));
+        let line_num = line_num_minus_one + 1;
+        let line = try!(line_rslt.map_err(|e| format!("I/O Error: {}", e)));
+
+        match parse_line(line.as_str()) {
+            Ok((Some(entry), rest)) => {
+                entries.push(entry);
+                if rest.len() > 0 {
+                    println!("Parsed an entry on line {}, but had leftover content: {:?}", line_num, rest);
+                }
+            },
+            Ok((None, rest)) => {
+                if rest.len() > 0 {
+                    println!("Parsed a comment or blank line  on line {}, but had leftover content: {:?}", line_num, rest);
+                }
+            },
+            Err(err) => {
+                return Err(format!("Parse error on line {}: {}", line_num, err));
+            }
+        }
     }
 
     Ok(entries)
@@ -17,12 +35,11 @@ pub fn parse<R: Read>(input: &mut R) -> Result<Vec<MagicEntry>, String> {
 
 type CombParseResult<I, O> = Result<(O, I), ParseError<I>>;
 
-#[allow(dead_code)]
 fn parse_line<I>(line: I) -> CombParseResult<I, Option<MagicEntry>>
     where I: Stream<Item = char> + Clone
 {
     let blank = (spaces(), eof()).map(|_| None);
-    let comment = (spaces(), token('#'), many::<Vec<char>, _>(try(any()))).map(|_| None);
+    let comment = (spaces(), token('#'), many::<String, _>(try(any()))).map(|_| None);
     let mut ignorer = try(blank).or(comment);
 
     match ignorer.parse(line.clone()) {
@@ -36,11 +53,26 @@ fn parse_line<I>(line: I) -> CombParseResult<I, Option<MagicEntry>>
     })
 }
 
-#[allow(dead_code)]
 fn entry<I>(_line: I) -> CombParseResult<I, MagicEntry>
     where I: Stream<Item = char>
 {
     unimplemented!();
+}
+
+#[allow(dead_code)]
+fn unsigned_number<I>(line: I) -> CombParseResult<I, u64>
+    where I: Stream<Item = char>
+{
+    let hex = (string("0x"), many1::<String, _>(hex_digit()))
+        .map(|(_, hex_str)| {
+            u64::from_str_radix(&hex_str, 16).unwrap()
+        });
+    let dec = many1::<String, _>(digit())
+        .map(|dec_str| {
+            u64::from_str(&dec_str).unwrap()
+        });
+
+    try(hex).or(dec).parse(line)
 }
 
 #[cfg(test)]
@@ -53,5 +85,15 @@ mod tests {
     #[test]
     fn ignores_comments() {
         assert_eq!(Ok((None, "")), super::parse_line(" \t# Comment"));
+    }
+
+    #[test]
+    fn numbers() {
+        assert_eq!(Ok((3_551_379_183, "")), super::unsigned_number("0xd3adBEEF"));
+        assert_eq!(Ok((314, "")), super::unsigned_number("314"));
+        assert_eq!(Ok((0, "")), super::unsigned_number("0"));
+
+        // Should this actually be octal?
+        assert_eq!(Ok((314, "")), super::unsigned_number("0314"));
     }
 }
