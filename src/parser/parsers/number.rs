bf5c0278efff679283733e62071d8e203192cb5b
Andrew Watts <ahwatts@gmail.com>
2017-04-06 20:14:33 -0400

    More exhaustive integer-parsing tests. That fail.

--- src/parser/parsers/number.rs
+++ src/parser/parsers/number.rs
@@ -239,50 +239,122 @@ fn parse_str_radix<N>(num_str: &str, radix: u32) -> Result<N, String>
 
 #[cfg(test)]
 mod tests {
-    use combine::Parser;
-    use data_type;
-    use endian::Endian;
+    // use data_type;
+    // use endian::Endian;
 
-    #[test]
-    fn integers() {
-        assert_eq!(Ok((893, "")), super::dec_integer::<u16, _>().parse("893"));
-        assert_eq!(Ok((3_551_379_183, "")), super::hex_integer::<u32, _>().parse("d3adBEEF"));
-        assert_eq!(Ok((78, "")), super::oct_integer::<i8, _>().parse("116"));
+    macro_rules! test_uint_boundaries {
+        ($bits:expr, $int_type:ident, $format_str:expr) => {
+            let bits_m1: $int_type = $bits - 1;
 
-        assert_eq!(Ok((20, "")), super::integer::<i32, _>().parse("20"));
-        assert_eq!(Ok((20, "")), super::integer::<i32, _>().parse("0x14"));
-        assert_eq!(Ok((20, "")), super::integer::<i32, _>().parse("024"));
+            let zero: $int_type = 0;
+            let half_m1: $int_type = (1 << bits_m1) - 1;
+            let half: $int_type = half_m1 + 1;
+            let max: $int_type = $int_type::max_value();
 
-        assert_eq!(Ok((-20, "")), super::integer::<i32, _>().parse("-20"));
-        assert_eq!(Ok((-20, "")), super::integer::<i32, _>().parse("-0x14"));
-        assert_eq!(Ok((-20, "")), super::integer::<i32, _>().parse("-024"));
+            let zero_str = format!($format_str, zero);
+            let half_m1_str = format!($format_str, half_m1);
+            let half_str = format!($format_str, half);
+            let max_str = format!($format_str, max);
 
-        assert!(super::hex_integer::<u16, _>().parse("1FFFF").is_err());
-        assert!(super::integer::<u32, _>().parse("-1025").is_err());
+            // println!("zero     = {:?}", zero_str);
+            // println!("half - 1 = {:?}", half_m1_str);
+            // println!("half     = {:?}", half_str);
+            // println!("max      = {:?}", max_str);
 
-        assert_eq!(Ok((0, "")), super::integer::<i32, _>().parse("0"));
+            assert_eq!(Ok((zero, "")), integer::<$int_type, _>().parse(zero_str.as_str()));
+            assert_eq!(Ok((half_m1, "")), integer::<$int_type, _>().parse(half_m1_str.as_str()));
+            assert_eq!(Ok((half, "")), integer::<$int_type, _>().parse(half_str.as_str()));
+            assert_eq!(Ok((max, "")), integer::<$int_type, _>().parse(max_str.as_str()));
+        }
     }
 
-    #[test]
-    fn integer_bytes() {
-        let dt = data_type::DataType::Long { endian: Endian::Native, signed: true };
-        let bytes = vec![ 20, 0, 0, 0 ];
-        assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("20"));
-        assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("0x14"));
-        assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("024"));
+    macro_rules! test_int_boundaries {
+        ($bits:expr, $int_type:ident, $format_str:expr) => {
+            let zero: $int_type = 0;
+            let max_pos: $int_type = $int_type::max_value();
+            let max_neg: $int_type = $int_type::min_value();
+            let minus_1: $int_type = zero - 1;
 
-        let bytes = vec![ 236, 255, 255, 255 ];
-        assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("-20"));
-        assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("-0x14"));
-        assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("-024"));
+            let zero_str = format!($format_str, zero);
+            let max_pos_str = format!($format_str, max_pos);
+            let max_neg_str = format!($format_str, max_neg);
+            let minus_1_str = format!($format_str, minus_1);
 
-        let dt = data_type::DataType::Quad { endian: Endian::Native, signed: false };
-        let bytes = vec![ 0x00, 0x40, 0x10, 0x00, 0x00, 0x02, 0x00, 0x10 ];
-        assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("0x1000020000104000"));
-        assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("0100000040000004040000"));
-        assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("1152923703631167488"));
+            // println!("zero = {:?}", zero_str);
+            // println!("max+ = {:?}", max_pos_str);
+            // println!("max- = {:?}", max_neg_str);
+            // println!("-1   = {:?}", minus_1_str);
 
-        let bytes = vec![ 0, 0, 0, 0, 0, 0, 0, 0 ];
-        assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("0"));
+            assert_eq!(Ok((zero, "")), integer::<$int_type, _>().parse(zero_str.as_str()));
+            assert_eq!(Ok((max_pos, "")), integer::<$int_type, _>().parse(max_pos_str.as_str()));
+            assert_eq!(Ok((max_neg, "")), integer::<$int_type, _>().parse(max_neg_str.as_str()));
+            assert_eq!(Ok((minus_1, "")), integer::<$int_type, _>().parse(minus_1_str.as_str()));
+        }
     }
+
+    macro_rules! test_int {
+        ($mod_name:ident, $bits:expr, $uint_type:ident, $int_type:ident) => {
+            mod $mod_name {
+                use super::super::*;
+
+                #[test]
+                fn parse_unsigned_from_octal() {
+                    test_uint_boundaries!($bits, $uint_type, "0{:o}");
+                }
+
+                #[test]
+                fn parse_signed_from_octal() {
+                    test_int_boundaries!($bits, $int_type, "0{:o}");
+                }
+
+                #[test]
+                fn parse_unsigned_from_decimal() {
+                    test_uint_boundaries!($bits, $uint_type, "{}");
+                }
+
+                #[test]
+                fn parse_signed_from_decimal() {
+                    test_int_boundaries!($bits, $int_type, "{}");
+                }
+
+                #[test]
+                fn parse_unsigned_from_hex() {
+                    test_uint_boundaries!($bits, $uint_type, "0x{:x}");
+                }
+
+                #[test]
+                fn parse_signed_from_hex() {
+                    test_int_boundaries!($bits, $int_type, "0x{:x}");
+                }
+            }
+        }
+    }
+
+    test_int!(byte_int, 8, u8, i8);
+    test_int!(short_int, 16, u16, i16);
+    test_int!(long_int, 32, u32, i32);
+    test_int!(long_long_int, 64, u64, i64);
+
+    // #[test]
+    // fn integer_bytes() {
+    //     let dt = data_type::DataType::Long { endian: Endian::Native, signed: true };
+    //     let bytes = vec![ 20, 0, 0, 0 ];
+    //     assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("20"));
+    //     assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("0x14"));
+    //     assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("024"));
+
+    //     let bytes = vec![ 236, 255, 255, 255 ];
+    //     assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("-20"));
+    //     assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("-0x14"));
+    //     assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("-024"));
+
+    //     let dt = data_type::DataType::Quad { endian: Endian::Native, signed: false };
+    //     let bytes = vec![ 0x00, 0x40, 0x10, 0x00, 0x00, 0x02, 0x00, 0x10 ];
+    //     assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("0x1000020000104000"));
+    //     assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("0100000040000004040000"));
+    //     assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("1152923703631167488"));
+
+    //     let bytes = vec![ 0, 0, 0, 0, 0, 0, 0, 0 ];
+    //     assert_eq!(Ok((bytes.clone(), "")), super::integer_bytes(&dt).parse("0"));
+    // }
 }
